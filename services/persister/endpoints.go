31c784958311b867c63a14c552652fc2fba98066
Cihangir SAVAS <cihangir@savas.io>
2017-09-22 17:38:03 -0700

    services/persister: implement persister service for persisting the data from redis to mongo

---
+++ services/persister/endpoints.go
@@ -0,0 +1,81 @@
+package persister
+
+import (
+	"context"
+	"net/url"
+	"strings"
+
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+)
+
+// Endpoints collects all of the endpoints that compose a persister service.
+type Endpoints struct {
+	ProcessEndpoint endpoint.Endpoint
+}
+
+// MakeServerEndpoints returns an Endpoints struct where each endpoint invokes
+// the corresponding method on the provided service. Useful in a persister
+// server.
+func MakeServerEndpoints(s Service) Endpoints {
+	return Endpoints{
+		ProcessEndpoint: MakeProcessEndpoint(s),
+	}
+}
+
+// MakeClientEndpoints returns an Endpoints struct where each endpoint invokes
+// the corresponding method on the remote instance, via a transport/http.Client.
+// Useful in a persister client.
+func MakeClientEndpoints(instance string) (Endpoints, error) {
+	if !strings.HasPrefix(instance, "http") {
+		instance = "http://" + instance
+	}
+	tgt, err := url.Parse(instance)
+	if err != nil {
+		return Endpoints{}, err
+	}
+	tgt.Path = ""
+
+	options := []httptransport.ClientOption{}
+
+	// Note that the request encoders need to modify the request URL, changing
+	// the path and method. That's fine: we simply need to provide specific
+	// encoders for each endpoint.
+
+	return Endpoints{
+		ProcessEndpoint: httptransport.NewClient("POST", tgt, encodeProcessRequest, decodeProcessResponse, options...).Endpoint(),
+	}, nil
+}
+
+// Process implements Service. Primarily useful in a client.
+func (e Endpoints) Process(ctx context.Context, p Profile) error {
+	request := ProcessRequest{Profile: p}
+	response, err := e.ProcessEndpoint(ctx, request)
+	if err != nil {
+		return err
+	}
+	resp := response.(ProcessResponse)
+	return resp.Err
+}
+
+// MakeProcessEndpoint returns an endpoint via the passed service.
+// Primarily useful in a server.
+func MakeProcessEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(ProcessRequest)
+		e := s.Process(ctx, req.Profile)
+		return ProcessResponse{Err: e}, nil
+	}
+}
+
+// ProcessRequest holds the request data for the Process handler
+type ProcessRequest struct {
+	Profile Profile
+}
+
+// ProcessResponse holds the response data for the Process handler
+type ProcessResponse struct {
+	Err error `json:"err,omitempty"`
+}
+
+func (r ProcessResponse) error() error { return r.Err }
