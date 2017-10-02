00a516f9f1b703d84da2c3351a2b202b44bf8675
Cihangir SAVAS <cihangir@savas.io>
2017-10-02 16:36:37 -0700

    pkg: do not use redigo directly

--- pkg/redis.go
+++ pkg/redis.go
@@ -1,43 +1,21 @@
 package pkg
 
 import (
-	"time"
-
-	"github.com/garyburd/redigo/redis"
+	redigo "github.com/garyburd/redigo/redis"
+	"github.com/koding/redis"
 )
 
 // NewRedisPool creates a redis connection pool
-func NewRedisPool(server string, options ...redis.DialOption) (*redis.Pool, error) {
-	if len(options) == 0 {
-		options = []redis.DialOption{
-			redis.DialReadTimeout(5 * time.Second),
-			redis.DialWriteTimeout(time.Second),
-			redis.DialConnectTimeout(time.Second),
-		}
-	}
-
-	p := &redis.Pool{
-		MaxIdle:     3,
-		MaxActive:   1000,
-		IdleTimeout: 30 * time.Second,
-		Dial: func() (redis.Conn, error) {
-			c, err := redis.Dial("tcp", server, options...)
-			if err != nil {
-				return nil, err
-			}
-
-			return c, err
-		},
-		TestOnBorrow: func(c redis.Conn, t time.Time) error {
-			_, err := c.Do("PING")
-			return err
-		},
-	}
-
-	// test if the connection is working or not.
-	if err := p.Get().Close(); err != nil {
+func NewRedisPool(server string, options ...redigo.DialOption) (*redis.RedisSession, error) {
+	r, err := redis.NewRedisSession(&redis.RedisConf{Server: server})
+	if err != nil {
 		return nil, err
 	}
 
-	return p, nil
+	// test if the connection is working or not.
+	if err := r.Pool().Get().Close(); err != nil {
+		return nil, err
+	}
+
+	return r, nil
 }
