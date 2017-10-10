639b20851ce04c3e93305d6d9fa99416c594bfbb
Cihangir SAVAS <cihangir@savas.io>
2017-10-10 22:28:34 +0300

    services/compactor: refactor functions for better readability

--- services/compactor/service.go
+++ services/compactor/service.go
@@ -58,56 +58,30 @@ func (c *compactorService) Process(ctx context.Context, p ProcessRequest) error 
 	return nil
 }
 
-func (c *compactorService) process(redisConn *redis.RedisSession, suffix string, tr time.Time) error {
-	timedSuffix := suffix + ":" + strconv.FormatInt(tr.Unix(), 10)
-	return c.withLock(redisConn, timedSuffix, func(srcMember string) error {
-		segment := tr.Add(-(time.Hour / 2)).Round(time.Hour).Unix()
+const seperator = ":"
 
-		hashSetPrefix := "hset:counter:" + timedSuffix + ":"
-		hourlyDestHashSetPrefix := "hset:counter:" + suffix + ":" + strconv.FormatInt(segment, 10)
+func (c *compactorService) process(redisConn *redis.RedisSession, directionSuffix string, tr time.Time) error {
+	var (
+		currentSegmentSuffix = directionSuffix + seperator + strconv.FormatInt(tr.Unix(), 10)
+		hourlySegmentSuffix  = directionSuffix + seperator + strconv.FormatInt(tr.Add(-(time.Hour/2)).Round(time.Hour).Unix(), 10)
 
-		hashSetName := hashSetPrefix + srcMember
-		destHashSetName := hourlyDestHashSetPrefix + srcMember
+		currentHashSetPrefix = "hset:counter:" + currentSegmentSuffix + seperator
+		hourlyHashSetPrefix  = "hset:counter:" + hourlySegmentSuffix + seperator
+		queueName            = "set:counter:" + currentSegmentSuffix
+	)
 
-		fns, err := redigo.Int64Map(redisConn.HashGetAll(hashSetName))
-		if err == redis.ErrNil {
-			c.app.ErrorLog("msg", "item was in the queue but the corresponding values does not exist as hash map")
-			return nil
-		}
-
-		if err != nil {
-			return err
-		}
-
-		if len(fns) == 0 {
-			return nil
-		}
-
-		if err = c.merge(redisConn, fns, destHashSetName); err != nil {
-			return err
-		}
-
-		res, err := redisConn.Del(hashSetName)
-		if err == redis.ErrNil {
-			c.app.Logger.Log("msg", "we should be able to delete the counter here, but failed.", "name", hashSetName)
-			return nil
-		}
-		if err != nil {
-			return err
-		}
-
-		if res == 0 {
-			c.app.Logger.Log("msg", "we should be able to delete the counter hash map here, but failed.")
-		}
-
-		return nil
+	return c.withLock(redisConn, queueName, func(srcMember string) error {
+		var (
+			source = currentHashSetPrefix + srcMember
+			target = hourlyHashSetPrefix + srcMember
+		)
+		return c.merge(redisConn, source, target)
 	})
-
 }
 
-func (c *compactorService) withLock(redisConn *redis.RedisSession, suffix string, fn func(srcMember string) error) error {
-	queueName := "set:counter:" + suffix
-
+// withLock gets an item from the current segment's item set and passes it to
+// the given processor function. After getting a response from the processor a successfull
+func (c *compactorService) withLock(redisConn *redis.RedisSession, queueName string, fn func(srcMember string) error) error {
 	srcMember, err := redisConn.RandomSetMember(queueName)
 	if err == redis.ErrNil {
 		return nil // we dont have any, so nothing to do.
@@ -123,34 +97,82 @@ func (c *compactorService) withLock(redisConn *redis.RedisSession, suffix string
 	if err != nil {
 		return err
 	}
+
 	// 1 if the element is moved. 0 if the element is not a member of source
 	// and no operation was performed.
 	if res != 1 {
 		c.app.InfoLog("msg", "we tried to move a current member to processing queue but failed, someone has already moved the item in the mean time...")
-		return c.withLock(redisConn, suffix, fn)
+		return c.withLock(redisConn, queueName, fn)
 	}
 
 	fnErr := fn(srcMember)
 
+	if fnErr != nil {
+		_, err = redisConn.MoveSetMember(processingQueueName, queueName, srcMember)
+		if err != nil {
+			c.app.ErrorLog("msg", "error while trying to put to item back to process set after an unseccesful operation", "err", err.Error())
+		}
+
+		return fnErr
+	}
+
 	res, err = redisConn.RemoveSetMembers(processingQueueName, srcMember)
 	if err == redis.ErrNil {
 		c.app.ErrorLog("msg", "we should be able to delete from the processing set here, but failed.")
-		return fnErr
+		return err
 	}
 
 	if err != nil {
 		c.app.ErrorLog("msg", err.Error())
-		return fnErr
+		return err
 	}
 
 	if res == 0 {
 		c.app.ErrorLog("msg", "we should be able to delete from the processing set here, but failed.")
 	}
 
-	return fnErr
+	return nil
 }
 
-func (c *compactorService) merge(redisConn *redis.RedisSession, fns map[string]int64, destHashSetName string) error {
+// merge merges the source hash map values to the target, then deletes the
+// source hash map from the server.
+func (c *compactorService) merge(redisConn *redis.RedisSession, source, target string) error {
+	fns, err := redigo.Int64Map(redisConn.HashGetAll(source))
+	if err == redis.ErrNil {
+		c.app.ErrorLog("msg", "item was in the queue but the corresponding values does not exist as hash map")
+		return nil
+	}
+
+	if err != nil {
+		return err
+	}
+
+	if len(fns) == 0 {
+		return nil
+	}
+
+	if err = c.incrementMapValues(redisConn, target, fns); err != nil {
+		return err
+	}
+
+	res, err := redisConn.Del(source)
+	if err != redis.ErrNil {
+		c.app.Logger.Log("msg", "we should be able to delete the counter here, but failed.", "err", err.Error())
+		return nil
+	}
+
+	if err != nil {
+		return err
+	}
+
+	if res == 0 {
+		c.app.Logger.Log("msg", "we should be able to delete the counter hash map here, but failed. someone might already have deleted it..")
+	}
+
+	return nil
+}
+
+func (c *compactorService) incrementMapValues(redisConn *redis.RedisSession, target string, fns map[string]int64) error {
 	conn := redisConn.Pool().Get()
 	defer conn.Close()
 
@@ -161,7 +183,7 @@ func (c *compactorService) merge(redisConn *redis.RedisSession, fns map[string]i
 	}
 
 	for fn, val := range fns {
-		if _, err := conn.Do("HINCRBY", redisConn.AddPrefix(destHashSetName), fn, val); err != nil {
+		if _, err := conn.Do("HINCRBY", redisConn.AddPrefix(target), fn, val); err != nil {
 			return err
 		}
 	}
