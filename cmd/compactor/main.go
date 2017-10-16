14973221b070f28a5bbe1d0c0a3f392a5c1e818b
Cihangir SAVAS <cihangir@savas.io>
2017-10-16 15:46:41 +0300

    services/compactor: add process as cron job

--- cmd/compactor/main.go
+++ cmd/compactor/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"net/http"
+	"time"
 
 	"github.com/go-kit/kit/log"
 	"github.com/koding/ropecount/pkg"
@@ -10,7 +12,7 @@ import (
 )
 
 func main() {
-	name := "compator"
+	name := "compactor"
 	conf := flag.NewFlagSet(name, flag.ExitOnError)
 
 	pkg.AddHTTPConf(conf)
@@ -30,5 +32,21 @@ func main() {
 		h = compactor.MakeHTTPHandler(s, log.With(app.Logger, "component", "HTTP"))
 	}
 
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		t := time.NewTicker(time.Minute)
+		defer t.Stop()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-t.C:
+				if err := s.Process(ctx, compactor.ProcessRequest{StartAt: time.Now().UTC()}); err != nil {
+					app.ErrorLog("err", err.Error())
+				}
+			}
+		}
+	}()
 	app.Logger.Log("exit", <-app.Listen(h))
+	cancel()
 }
